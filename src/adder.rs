d990fbe617b28beaf46e781c63cf853436a8b11f
ygarrot <garrot.yoan1@gmail.com>
2022-02-08 15:48:41 +0100

    adder

--- src/adder.rs
+++ src/adder.rs
@@ -1,4 +1,15 @@
 // fn adder(a: u32, b: u32) -> u32;
+
+pub fn inc(i: u32) -> u32 {
+    if (i & 1) == 0 {
+        return i | 1;
+    }
+    return inc(i >> 1) << 1;
+}
+
 pub fn adder(a: u32, b: u32) -> u32 {
-    a + b
+    if b == 0 {
+        return a;
+    }
+    adder(inc(a), b - 1)
 }
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ mod tests {
     #[test]
     fn adder() {
         assert_eq!(3, adder::adder(1, 2));
+        assert_eq!(7, adder::adder(5, 2));
+        assert_eq!(8, adder::adder(6, 2));
     }
 
 }
